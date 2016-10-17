3264ed5af69dc34487b3b1981a53468482bd96ce
davidPies <2014119@edtools.psd401.net>
2016-10-17 11:50:06 -0700

    Update spongebobmod_1.java

--- spongebobmod_1.java
+++ spongebobmod_1.java
@@ -45,7 +45,9 @@ public class trumpmod
     
     	//public static wheatcannon1 wheatcannon1;
     	public static Item pants;
-    	public static ArmorMaterial spongepants = EnumHelper.addArmorMaterial("spongepants", "spongebobmod_1:spongepants", 10, new int[]{2, 0, 0, 2}, 15, SoundEvents.BLOCK_CLOTH_HIT, 0.0F);
+    	public static Item patty;
+
+    public static ArmorMaterial spongepants = EnumHelper.addArmorMaterial("spongepants", "spongebobmod_1:spongepants", 10, new int[]{2, 0, 0, 2}, 15, SoundEvents.BLOCK_CLOTH_HIT, 0.0F);
     	
     	@SidedProxy(clientSide="spongebobmod_1.client", serverSide="spongebobmod_1.server")
     public static sharedproxy proxy;
@@ -68,7 +70,8 @@ public class trumpmod
     		}});
     	//EntityRegistry.registerModEntity(trump.class, "trump", 0, MODID, 15, 5, true);
     	//EntityRegistry.registerEgg(trump.class, 0, 1000);
-    	GameRegistry.registerItem(pants = new armor("spongebobmod_1:spongepants", "spongepants", spongepants, 0, EntityEquipmentSlot.LEGS), "spongepants");
+    	GameRegistry.registerItem(patty = new patty(), "patty");
+        GameRegistry.registerItem(pants = new armor("spongebobmod_1:spongepants", "spongepants", spongepants, 0, EntityEquipmentSlot.LEGS), "spongepants");
     	//GameRegistry.registerItem(money = new money(4, 1, false), "money");
     	//GameRegistry.registerItem(snatcher = new snatcher(), "snatcher");
     	//GameRegistry.registerItem(spring = new spring(), "spring");
@@ -78,7 +81,7 @@ public class trumpmod
     	//GameRegistry.addRecipe(new ItemStack(trumpmod.spring, 4), new Object[] {"x  ", " x ", "  x", 'x', Items.STRING});
     	//GameRegistry.addRecipe(new ItemStack(trumpmod.handle, 4), new Object[] {"xx", "xx", 'x', Items.STICK});
     	//GameRegistry.addRecipe(new ItemStack(trumpmod.grabber, 4), new Object[] {" x", "xx", 'x', Items.STICK});
-    	
+    	Minecraft.getMinecraft().getRenderItem().getItemModelMesher().register(patty, 0, new ModelResourceLocation(MODID + ":" + patty.getUnlocalizedName().substring(5), "inventory"));
     	Minecraft.getMinecraft().getRenderItem().getItemModelMesher().register(spongepants, 0, new ModelResourceLocation(MODID + ":" + spongepants.getUnlocalizedName().substring(5), "inventory"));
     	//Minecraft.getMinecraft().getRenderItem().getItemModelMesher().register(money, 0, new ModelResourceLocation(MODID + ":" + money.getUnlocalizedName().substring(5), "inventory"));
     	//Minecraft.getMinecraft().getRenderItem().getItemModelMesher().register(snatcher, 0, new ModelResourceLocation(MODID + ":" + snatcher.getUnlocalizedName().substring(5), "inventory"));
